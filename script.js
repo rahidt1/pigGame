8e5a0bcf53840daee6465c94cbf500393c9f2de0
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2022-04-12 19:51:29 +0600

    Rolling dice and swich active player.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generate dice
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
+
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // Check for rolled 1: if true, swich player
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
